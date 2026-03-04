cf15be7c4e877d6534efa9584a11ab8c061f1a8f
cx <332147951@qq.com>
2026-03-04 13:11:59 +0800

    update

--- addons/pass_through_manager/Examples/move_icon.gd
+++ addons/pass_through_manager/Examples/move_icon.gd
@@ -4,8 +4,13 @@ var is_dragging = false
 var initial_position = Vector2.ZERO
 var initial_mouse_position = Vector2.ZERO
 @onready var collision_polygon_2d: CollisionPolygon2D = $Area2D/CollisionPolygon2D
+var last_global_position = Vector2.ZERO
+var last_global_scale = Vector2.ZERO
 func _ready() -> void:
     PassthroughManager.RegisterCollisionPolygon2DClickArea(collision_polygon_2d);
+    last_global_position = global_position
+    last_global_scale = global_scale
+
     var left_click = InputEventMouseButton.new()
     left_click.button_index = MOUSE_BUTTON_LEFT
     if !InputMap.has_action("click"):
@@ -23,6 +28,11 @@ func _process(delta: float) -> void:
             is_dragging = false
         
         global_position = get_global_mouse_position() - initial_mouse_position + initial_position
+
+    # 判定形状是否变化
+    if global_position != last_global_position or global_scale != last_global_scale:
+        last_global_position = global_position
+        last_global_scale = global_scale
         PassthroughManager.UpdateCollisionPolygon2DClickArea(collision_polygon_2d);
 
 func is_mouse_over() -> bool:
--- addons/pass_through_manager/Examples/test_pass_through_editor.cs
+++ addons/pass_through_manager/Examples/test_pass_through_editor.cs
@@ -15,12 +15,14 @@ public partial class test_pass_through_editor : Node2D
 
 	public override void _Draw()
 	{
+		GlobalPosition = new Vector2(0, 0);
 		DrawInternal(PassthroughManager.Instance.QuadTree.Root);
 	}
 
 	private void DrawInternal(QuadTreeNode node)
 	{
-		DrawRect(node.Rect, Colors.Green, false, 5);
+		var rect = new Rect2(node.Rect.Position / GlobalScale - GlobalPosition, node.Rect.Size / GlobalScale);
+		DrawRect(rect, Colors.Green, false, 5);
 		// GD.Print(node.Rect.Position, node.quadTreeItems.Count);
 		foreach (var child in node.children)
 		{
